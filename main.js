9717eda4f7a93412e77ebf84231d2741aff152ac
Arrt553 <77664237+Arrt553@users.noreply.github.com>
2021-07-20 07:36:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ function setup(){
 }
 function draw(){
     image(video,0,0,400,390);
-    classifier.classify(video,gotResults);
+    classifier.classify(video,gotResult);
 }
- function gotResults(error,results){
+ function gotResult(error,results){
      if(error){
          console.error(error);
      }
      else{
          console.log(results);
-         document.getElementById("results_object_name").innerHTML = results[0].label;
+         document.getElementById("result_object_name").innerHTML = results[0].label;
          document.getElementById("result_object_accuracy").innerHTML = results[0].label.confidence.toFixed(2);
      }
  }
